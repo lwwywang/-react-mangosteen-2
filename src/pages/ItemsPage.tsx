34eb26b5c1c69502fbd17ccdc00740b8c60d0c6d
lwwywang <liwenwang4work@163.com>
2024-02-21 11:06:59 +0800

    deploy

---
+++ src/pages/ItemsPage.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { AddItemFloatButton } from '../components/AddItemFloatButton'
+import type { TimeRange } from '../components/TimeRangePicker'
+import { TimeRangePicker } from '../components/TimeRangePicker'
+import { TopMenu } from '../components/TopMenu'
+import { TopNav } from '../components/TopNav'
+import { menuContext } from '../contexts/menuContext'
+import { useMenuStore } from '../stores/useMenuStore'
+import { ItemsList } from './ItemsPage/ItemsList'
+import { ItemsSummary } from './ItemsPage/ItemsSummary'
+
+const Div = styled.div`
+  background: linear-gradient(0deg, rgba(143,76,215,1) 0%, rgba(92,51,190,1) 100%);
+`
+
+export const ItemsPage: React.FC = () => {
+  const [timeRange, setTimeRange] = useState<TimeRange>('thisMonth')
+  const [items] = useState<Item[]>([
+    {
+      id: 1,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    }, {
+      id: 2,
+      kind: 'incomes',
+      amount: 1000,
+      user_id: 1,
+      tag_ids: [1],
+      happen_at: '2021-01-01T00:00:00.000Z',
+      created_at: '2021-01-01T00:00:00.000Z',
+      updated_at: '2021-01-01T00:00:00.000Z',
+    }
+  ])
+  const { visible } = useMenuStore()
+  return (
+    <div>
+      <Div>
+        <TopNav />
+        <TimeRangePicker selected={timeRange} onSelected={setTimeRange} />
+      </Div>
+      <ItemsSummary />
+      <ItemsList items={items} />
+      <AddItemFloatButton />
+      {visible ? <TopMenu /> : null}
+    </div>
+  )
+}
